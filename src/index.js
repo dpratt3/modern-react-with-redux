68da9b5c495369b755c47b1bc4f48345980f5404
David Pratt <david@midnightmechanism.com>
2023-07-07 23:10:39 -0500

    Finish the working example from section 12

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
 // import React and React-Dom libraries
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 // Get a reference to the div with id 'root'
-const el = document.getElementById('root')
+const el = document.getElementById("root");
 
 // Tell React to take control of that element
 const root = ReactDOM.createRoot(el);
 
 // Create a component
-function App(){
-	return <h1>Hi There!</h1>
+function App() {
+  let message = "Bye there!";
+  if (Math.random() > 0.5) message = "Hi there!";
+  return <h1>{message}</h1>;
 }
 
 // Show a component on the screen
-root.render(<App />)
+root.render(<App />);
