8b6925929abfc45c5eeceef0b2505587b240d2d5
David <davidpratt402@gmail.com>
2023-07-09 15:12:40 -0500

    Put in some bulma

--- pdas/src/App.js
+++ pdas/src/App.js
@@ -8,9 +8,9 @@ function App() {
   return (
     <div>
       <div>Personal Digital Assistant</div>
-      <ProfileCard title="Alexa" handle="@alexa99" image={AlexaImage} />
-      <ProfileCard title="Cortana" handle="@cortan32" image={CortanaImage} />
-      <ProfileCard title="Siri" handle="@siri01" image={SiriImage} />
+        <ProfileCard title="Alexa" handle="@alexa99" image={AlexaImage} />
+        <ProfileCard title="Cortana" handle="@cortan32" image={CortanaImage} />
+        <ProfileCard title="Siri" handle="@siri01" image={SiriImage} />
     </div>
   );
 }
