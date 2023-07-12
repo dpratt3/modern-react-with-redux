9d37ee8308a93584d2bd26b109d3c11244b93e05
David <davidpratt402@gmail.com>
2023-07-11 19:56:14 -0500

    Finish throught No. 72

--- pics/src/api.js
+++ pics/src/api.js
@@ -1,16 +1,16 @@
 import axios from "axios";
 
-const searchImages = async() => {
+const searchImages = async(term) => {
     const response = await axios.get("https://api.unsplash.com/search/photos", {
         headers: {
             Authorization: "Client-ID bU1TkeE3H23UDKbGswfd4KnE5QjhGzsGkaeW4ok0TpU",
         },
         params: {
-            query: 'cars',
+            query: term,
         },
     });
-    console.log(response)
-    return response;
+
+    return response.data.results;
 };
 
 export default searchImages;
--- pics/src/components/SearchBar.js
+++ pics/src/components/SearchBar.js
@@ -1,5 +1,24 @@
-function SearchBar() {
-  return <div>SearchBar</div>;
+import { useState } from "react";
+function SearchBar({onSubmit}) {
+  const [term, setTerm] = useState('');
+
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+    onSubmit(term)
+  }
+
+  const handleChange = (event) => {
+    setTerm(event.target.value.replace(/[a-z]/, ''));
+  };
+
+  return (
+  <div>
+    <form onSubmit={handleFormSubmit}>
+      Confirm your search: {term}
+      <input value={term} onChange={handleChange}/>
+      {term.length < 3 && 'Term must be longer than 3 characters'}
+    </form>
+  </div>);
 }
 
 export default SearchBar;
